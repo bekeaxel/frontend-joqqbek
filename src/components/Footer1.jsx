686733a5e2930272b2a00f5b1596e8ea4c590bac
Joaquim Gouveia <joaquimgouveia2000@gmail.com>
2023-08-05 18:57:48 +0200

    Just messing around pt3

---
+++ src/components/Footer1.jsx
@@ -0,0 +1,16 @@
+import "../styles/footer1.css"
+
+const footer = () => {
+    return (
+        <div className ="footer-props">
+            <div className="col-1">
+                <h3>Useful Links</h3>
+                <link to={"/submit"}>Search</link>
+            </div>
+            <div className="col-2">
+                
+            </div>
+            <div className="col-3"></div>
+        </div>
+    )
+}
