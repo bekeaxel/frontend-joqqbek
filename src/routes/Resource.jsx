dfc953d45b6d93b08404e815fed5c7a4c2ed7733
axel beke <axel@beke.se>
2023-08-17 23:42:41 +0200

    added layout to pdf view

--- src/routes/Resource.jsx
+++ src/routes/Resource.jsx
@@ -1,10 +1,17 @@
 import { Viewer } from '@react-pdf-viewer/core';
-import {defaultPlugin} from '@react-pdf-viewer/core'
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import '../styles/resource.css'
 
 const Resource = () => {
-    return <div className="resource-container">
-       <Viewer fileUrl={'https://pdfobject.com/pdf/sample.pdf'} />;
+    const defaultLayout = defaultLayoutPlugin(); 
+    return <div className='resource-container'>
+
+        <div className="pdf-resource-container">
+            <h2>title of pdf maybe some other stuff as well</h2>
+            <Viewer fileUrl={'https://pdfobject.com/pdf/sample.pdf'} plugins={[defaultLayout]} />;
+        </div>
     </div>
 }
 
